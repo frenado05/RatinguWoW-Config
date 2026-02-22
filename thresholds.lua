b9f8bf038c35aa0ffae60b74308d66bc07b43ae2
Kyckax <kyckax@gmail.com>
2026-02-22 15:13:57 +0200

    UPD

--- thresholds.lua
+++ thresholds.lua
@@ -1,18 +1,22 @@
 -- thresholds.lua
--- Отдельный файл с порогами токенов для легкого обновления
+-- Auto-generated thresholds file
+-- Generated: 2026-02-22 15:11:49
+
 CustomThresholds = {
+
     [2] = {  -- 2х2
-		{ rating = 2607, reward = 4000, title = "Р1" },
-		{ rating = 2588, reward = 2500, title = "Гладиатор" },
-		{ rating = 2033, reward = 1500, title = "Дуэлянт" },
-		{ rating = 1664, reward = 1000, title = "Фаворит" },
-		{ rating = 1362, reward = 500, title = "Претендент" },
+        { rating = 2607, reward = 4000, title = "Р1" },
+        { rating = 2588, reward = 2500, title = "Гладиатор" },
+        { rating = 2033, reward = 1500, title = "Дуэлянт" },
+        { rating = 1664, reward = 1000, title = "Фаворит" },
+        { rating = 1362, reward = 500, title = "Претендент" },
     },
+
     [9] = {  -- Соло
-		{ rating = 2807, reward = 4000, title = "Р1" },
-		{ rating = 2645, reward = 2500, title = "Гладиатор" },
-		{ rating = 2340, reward = 1500, title = "Дуэлянт" },
-		{ rating = 2084, reward = 1000, title = "Фаворит" },
-		{ rating = 1650, reward = 500, title = "Претендент" },
+        { rating = 2877, reward = 4000, title = "Р1" },
+        { rating = 2645, reward = 2500, title = "Гладиатор" },
+        { rating = 2340, reward = 1500, title = "Дуэлянт" },
+        { rating = 2084, reward = 1000, title = "Фаворит" },
+        { rating = 1650, reward = 500, title = "Претендент" },
     },
 }
