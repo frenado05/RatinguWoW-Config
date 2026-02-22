f75d7c205a4f5f5913c49a3d1ae1c349a568bbcf
Kyckax <kyckax@gmail.com>
2026-02-22 04:42:58 +0200

    test

---
+++ thresholds.lua
@@ -0,0 +1,18 @@
+-- thresholds.lua
+-- Отдельный файл с порогами токенов для легкого обновления
+CustomThresholds = {
+    [2] = {  -- 2х2
+		{ rating = 2607, reward = 5000, title = "Р1" },
+		{ rating = 2588, reward = 2500, title = "Гладиатор" },
+		{ rating = 2033, reward = 1500, title = "Дуэлянт" },
+		{ rating = 1664, reward = 1000, title = "Фаворит" },
+		{ rating = 1362, reward = 500, title = "Претендент" },
+    },
+    [9] = {  -- Соло
+		{ rating = 2807, reward = 4000, title = "Р1" },
+		{ rating = 2645, reward = 2500, title = "Гладиатор" },
+		{ rating = 2340, reward = 1500, title = "Дуэлянт" },
+		{ rating = 2084, reward = 1000, title = "Фаворит" },
+		{ rating = 1650, reward = 500, title = "Претендент" },
+    },
+}
