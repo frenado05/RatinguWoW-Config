398607cb63e850d251b59edc669160ec574be4a2
Kyckax <kyckax@gmail.com>
2026-02-26 13:26:29 +0200

    Auto-update thresholds 2026-02-26 13:26

--- thresholds.lua
+++ thresholds.lua
@@ -1,23 +1,23 @@
 -- thresholds.lua
 -- Auto-generated thresholds file
--- Generated: 2026-02-25 01:14:53
+-- Generated: 2026-02-26 13:26:29
 
 CustomThresholds = {
 
-    lastUpdate = "2026-02-25 01:14",
+    lastUpdate = "2026-02-26 13:26",
     [2] = {  -- 2х2
         { rating = 2690, reward = 4000, title = "Р1" },
-        { rating = 2595, reward = 2500, title = "Гладиатор" },
+        { rating = 2603, reward = 2500, title = "Гладиатор" },
         { rating = 2019, reward = 1500, title = "Дуэлянт" },
         { rating = 1640, reward = 1000, title = "Фаворит" },
-        { rating = 1335, reward = 500, title = "Претендент" },
+        { rating = 1332, reward = 500, title = "Претендент" },
     },
 
     [9] = {  -- Соло
-        { rating = 2859, reward = 4000, title = "Р1" },
-        { rating = 2705, reward = 2500, title = "Гладиатор" },
-        { rating = 2312, reward = 1500, title = "Дуэлянт" },
-        { rating = 2066, reward = 1000, title = "Фаворит" },
-        { rating = 1604, reward = 500, title = "Претендент" },
+        { rating = 2838, reward = 4000, title = "Р1" },
+        { rating = 2645, reward = 2500, title = "Гладиатор" },
+        { rating = 2281, reward = 1500, title = "Дуэлянт" },
+        { rating = 2036, reward = 1000, title = "Фаворит" },
+        { rating = 1612, reward = 500, title = "Претендент" },
     },
 }
